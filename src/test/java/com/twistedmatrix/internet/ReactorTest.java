14f99f7b57cf6e26d2d889740e806a3a2f218cc0
Mike Mattice <mike.mattice@gmail.com>
2019-02-27 13:04:12 -0600

    add test to callLater to verify ordering

--- src/test/java/com/twistedmatrix/internet/ReactorTest.java
+++ src/test/java/com/twistedmatrix/internet/ReactorTest.java
@@ -5,7 +5,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ReactorTest {
@@ -23,7 +25,7 @@ public class ReactorTest {
     }
 
     @Test
-    public void callLater() throws Throwable {
+    public void callLaterOneLater() throws Throwable {
         AtomicBoolean ranLater = new AtomicBoolean(false);
 
         reactor.callLater(0.01, () -> {
@@ -36,4 +38,24 @@ public class ReactorTest {
         assertTrue(ranLater.get());
     }
 
+    @Test
+    public void callLaterSeveralLater() throws Throwable {
+        AtomicInteger i = new AtomicInteger(0);
+
+        reactor.callLater(0.05, () -> {
+            int j = i.getAndAdd(1);
+            assertEquals(j, 5);
+        });
+
+        reactor.callLater(0.01, () -> {
+            int j = i.getAndSet(5);
+            assertEquals(j, 0);
+        });
+
+        reactor.callLater(0.1, () -> { reactor.stop();});
+
+        reactor.run();
+
+        assertEquals(i.get(), 6);
+    }
 }
